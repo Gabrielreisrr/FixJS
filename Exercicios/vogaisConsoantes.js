750a709e449c22a4f1b73b4874ebbb5c93faff1f
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2025-02-19 21:10:11 -0300

    exercicio de analisar vogais

---
+++ Exercicios/vogaisConsoantes.js
@@ -0,0 +1,13 @@
+const analisaVogais = (string) => {
+    const vogais = ["a", "e", "i", "o", "u"];
+    let x = "";
+    
+    for (let letra of string) {
+        if (vogais.includes(letra)) x += letra;
+    }
+    
+    return x;
+}
+
+console.log(analisaVogais("abcdeddvb")); // "ae"
+console.log(analisaVogais("javascript")); // "aai"
