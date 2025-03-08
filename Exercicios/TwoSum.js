2e9f8e711e803025e7ff45f4ff8c5464dd52d1aa
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2025-03-08 18:55:52 -0300

    att1

--- Exercicios/TwoSum.js
+++ Exercicios/TwoSum.js
@@ -1,9 +1,12 @@
 // exercio TwoSun LeetCode
 
-twoSum = (list, target) => {
-    for (let i = 0; i < list.length; i++) {
-        for (let j = 1 + 1; j < list.length; j++) {
-
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
         }
     }
+    return []; // Retorna um array vazio se não encontrar pares  
 };
