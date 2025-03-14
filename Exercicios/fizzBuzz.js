857aca42fb945e2ce2c4d66def3457d6c69e4842
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2025-03-14 20:00:48 -0300

    palindrome ex

--- Exercicios/fizzBuzz.js
+++ Exercicios/fizzBuzz.js
@@ -1,6 +1,6 @@
 const fizzBuss = () => {
     for (let i = 0; i <= 100; i++) {
-        console.log(i % 3 === 0 && i % 5 === 0 ? "fizzBuzz" : i % 3 === 0 ? "fizz" : i % 5 === 0 ? "buzz" : i);
+        console.log(i % 3 === 0 && i % 5 === 0 ? "fizzBuzz" : i % 3 === 0 ? "fizz" : i % 5 === 0 ? "buss" : i);
     }
 }
 
