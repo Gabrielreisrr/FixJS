49cf1ba1dd0517ec5706f065c6d0d6df9c259ff6
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2025-04-23 21:49:02 -0300

    ex6

--- ClassBase.js
+++ ClassBase.js
@@ -1,57 +1,55 @@
-// class Pessoa {
-//     constructor(nome, sobrenome){
-//         this.nome = nome;
-//         this.sobrenome = sobrenome;
-//     }
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
 
-//     nomeCompleto() {
-//         console.log(`${this.nome} ${this.sobrenome}`)
-//     }
-// }
-
-// const p1 = new Pessoa("Gabriel", "Reis");
-// p1.nomeCompleto();
-// console.log(p1)
-
-
-// getters e setters
-class Carro {
-    constructor(nome) {
-        this.nome = nome;
-        this._velocidade = 0;
-    }
-
-    set velocidade(valor) {
-        if (typeof valor !== 'number') return;
-        if (valor >= 100 || valor <= 0) return;
-        this._velocidade = valor
-    }
-
-    get velocidade() {
-        return this._velocidade;
-    }
-
-    acelerar() {
-        if (this._velocidade >= 100) return;
-        this._velocidade++;
-    }
-
-    freiar() {
-        if (this._velocidade <= 0) return;
-        this._velocidade--;
-    }
-
-    static aumentaVelocidade() {
-        return velocidade = 50
-    }
+  nomeCompleto() {
+    console.log(`${this.nome} ${this.sobrenome}`);
+  }
 }
 
-const c1 = new Carro('fusca')
+const p1 = new Pessoa("Gabriel", "Reis");
+p1.nomeCompleto();
+console.log(p1);
 
-// for(let i = 0; i <= 200; i++){
-//     c1.acelerar()
+// getters e setters
+// class Carro {
+//     constructor(nome) {
+//         this.nome = nome;
+//         this._velocidade = 0;
+//     }
+
+//     set velocidade(valor) {
+//         if (typeof valor !== 'number') return;
+//         if (valor >= 100 || valor <= 0) return;
+//         this._velocidade = valor
+//     }
+
+//     get velocidade() {
+//         return this._velocidade;
+//     }
+
+//     acelerar() {
+//         if (this._velocidade >= 100) return;
+//         this._velocidade++;
+//     }
+
+//     freiar() {
+//         if (this._velocidade <= 0) return;
+//         this._velocidade--;
+//     }
+
+//     static aumentaVelocidade() {
+//         return velocidade = 50
+//     }
 // }
 
-c1.velocidade = 1111
-console.log(c1.velocidade)
+// const c1 = new Carro('fusca')
 
+// // for(let i = 0; i <= 200; i++){
+// //     c1.acelerar()
+// // }
+
+// c1.velocidade = 1111
+// console.log(c1.velocidade)
